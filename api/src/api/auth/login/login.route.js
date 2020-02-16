baa0091d0ad663698027be2b9c1a972e1edfa837
Dmytro Skumin <dmytroskumin@Dmytros-MacBook-Pro.local>
2020-02-16 16:17:43 +0200

    starting arch

---
+++ api/src/api/auth/login/login.route.js
@@ -0,0 +1,8 @@
+import express from 'express';
+import Login_controller from './login.controller';
+
+const router = express.Router();
+
+router.post('/login', Login_controller.login);
+
+module.exports = router;

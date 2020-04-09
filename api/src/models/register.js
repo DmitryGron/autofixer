09c6f5de4c10467fecbfd96eadb1df5d02a76adf
dmytro.skumin <dmytro.skumin@28stone.com>
2020-04-09 18:00:39 +0300

    updating

--- api/src/models/register.js
+++ api/src/models/register.js
@@ -5,21 +5,47 @@ const Schema = mongoose.Schema;
 const userSchema = new Schema({
 	username: {
 		type: String,
-		required: true,
+		required: [true, 'Username required'],
+		min: [2, 'Username too short'],
+    	max: 32,
 	},
 	email: {
 		type: String,
-		required: true,
+		required: [true, 'Email required'],
+		validate: {
+			validator: function (email) {
+				return /^([\w-\.]+@([\w-]+\.)+[\w-]{2,4})?$/.test(email);
+			},
+			message: props => `${props.value} is not a valid email`
+		}
 	},
 	password: {
 		type: String,
-		required: true,
+		required: [true, 'Password required'],
+		min: [6, 'Password too weak'],
+    	max: 32,
 	},
-	isAdmin: {
+	phone: {
+		type: String,
+		validate: {
+		  validator: function(phone) {
+			return /\d{3}-\d{3}-\d{4}/.test(phone);
+		  },
+		  message: props => `${props.value} is not a valid phone number`
+		},
+		required: [true, 'User phone number required']
+	  },
+	  isAdmin: {
 		type: Boolean,
 		default: false,
 	},
 });
 
-export const register = mongoose.model('user', userSchema, 'users');
+userSchema.methods.generateAuthToken = function() {
+	return jwt.sign({ _id: this._id, isAdmin: this.isAdmin }, config.jwt.key, {
+		expiresIn: process.env.NODE_ENV == 'development' ? '20m' : '20m',
+	});
+};
+
+const register = mongoose.model('user', userSchema, 'users');
 module.exports = register;
